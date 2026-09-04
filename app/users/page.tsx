9d54a72aaa6dae1c7a32abccac8a3f3636ee61ab
Abolfazl-Edalati <a.g.e.tech@outlook.com>
2026-09-04 15:11:48 +0330

    Learn useRouter

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const users = [
   { id: "1", name: "Abolfazl" },
@@ -6,6 +9,16 @@ const users = [
 ];
 
 export default function UsersPage() {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push("/users/1");
+  };
+
+  const handleBack = () => {
+    router.back();
+  };
+
   return (
     <div className="space-y-6">
       <h1 className="font-bold text-xl">Users</h1>
@@ -17,6 +30,11 @@ export default function UsersPage() {
           </Link>
         ))}
       </ul>
+
+      <div className="flex gap-2 flex-col">
+        <button onClick={handleClick}>User 1</button>
+        <button onClick={handleBack}>Back</button>
+      </div>
     </div>
   );
 }
